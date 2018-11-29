5efda56f64a068ca8c1f3ab14f21f7465c907468
nitie <nitie@dianhun.cn>
2018-11-29 20:56:46 +0800

    加入了按键检测系统，现在可以正确的检测出玩家的按键输入

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -13,48 +13,122 @@ namespace GameLogic
         Attack03,
     }
 
-    public enum InputButton
-    {
-        A,
-        B,
-        S,
-    }
-
     public class InputManager : MonoBehaviour
     {
+        private Koreography controller;
+        private KoreographyTrack rhythmTrack;
+        private List<KoreographyEvent> rawEvents;
+
+        private int eventIndex;
+        private int rangeInSamples;
+
+        private bool isHit
+        {
+            get
+            {
+                return (Mathf.Abs(currentEvent.StartSample - DelayedSampleTime) <= rangeInSamples);
+            }
+        }
+
+        private bool isMissed
+        {
+            get
+            {
+                return DelayedSampleTime - currentEvent.StartSample >= rangeInSamples;
+            }
+        }
+
+        private KoreographyEvent currentEvent
+        {
+            get
+            {
+                return rawEvents[eventIndex];
+            }
+        }
+
+        [EventID]
+        public string eventID;
         public PlayerAnimState playerAnimState;
-
         public Animator playerAnimator;
+        public AudioSource audioCom;
 
-        public List<InputButton> userInputs;
+        [HideInInspector]
+        public List<KeyCode> userInputs;
+        [Range(50f, 150f)]
+        public int rangeInMS;
+
+        public int SampleRate
+        {
+            get
+            {
+                return controller.SampleRate;
+            }
+        }
+
+        public int DelayedSampleTime
+        {
+            get
+            {
+                return controller.GetLatestSampleTime() - (int)(audioCom.pitch * SampleRate);
+            }
+        }
+
+        private void UpdateInternalValues()
+        {
+            rangeInSamples = (int)(0.001f * rangeInMS * SampleRate);
+        }
 
         private void Start()
         {
-            Koreographer.Instance.RegisterForEvents("Bump", UserInput);
+            Koreographer.Instance.RegisterForEvents(eventID, UserInput);
+            eventIndex = 0;
+            controller = Koreographer.Instance.GetKoreographyAtIndex(0);
+            rhythmTrack = controller.GetTrackByID(eventID);
+            rawEvents = rhythmTrack.GetAllEvents();
+            Debug.LogError(rawEvents.Count);
+        }
+
+        private void Update()
+        {
+            UpdateInternalValues();
+
+            if (userInputs.Count == 8)
+            {
+                foreach (var key in userInputs)
+                {
+                    Debug.Log(key);
+                }
+                Debug.Log("================================");
+                userInputs.Clear();
+            }
+
+            if (isHit)
+            {
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    userInputs.Add(KeyCode.A);
+                    Debug.LogError("A");
+                    eventIndex++;
+                }
+                else if (Input.GetKeyDown(KeyCode.B))
+                {
+                    userInputs.Add(KeyCode.B);
+                    Debug.LogError("B");
+                    eventIndex++;
+                }          
+            }
+
+            if (isMissed)
+            {
+                userInputs.Add(KeyCode.Space);
+                Debug.LogError("=");
+                eventIndex++;
+            }
         }
 
         private void UserInput(KoreographyEvent koreoEvent)
         {
-            if (Input.GetKey(KeyCode.A))
-            {
-                userInputs.Add(InputButton.A);
-                Debug.LogError("A");
-            }
-            else if (Input.GetKey(KeyCode.B))
-            {
-                userInputs.Add(InputButton.B);
-                Debug.LogError("B");
-            }
-            else
-            {
-                userInputs.Add(InputButton.S);
-                Debug.LogError("S");
-            }
 
-            if (userInputs.Count == 8)
-            {
-                userInputs.Clear();
-            }
         }
 
         public void ChangeState(PlayerAnimState state)
